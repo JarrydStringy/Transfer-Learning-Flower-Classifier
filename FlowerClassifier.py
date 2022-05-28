8b023f838e765f9e9453e80d6484904074ca0eee
Jarryd <jarryd.stringfellow@gmail.com>
2022-05-28 12:26:07 +1000

    Test data added

--- FlowerClassifier.py
+++ FlowerClassifier.py
@@ -3,18 +3,15 @@ from fileinput import filename
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import layers
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications import imagenet_utils
-from PIL import Image
 import os
 
 
 def PrepareImage(file):
     img_path = os.path.join(os.path.dirname(__file__),
                             '../small_flower_dataset/')
-
-    # im = Image.open(img_path)
-    # im.show()
 
     # MobileNet expects 224x224 image sizes
     img = image.load_img(img_path + file, target_size=(224, 224))
@@ -25,13 +22,39 @@ def PrepareImage(file):
     return tf.keras.applications.mobilenet_v2.preprocess_input(img_array_expanded_dims)
 
 
+def New():
+    layer = keras.layers.Dense(3)
+
+    layer.build((None, 5))  # Create the weights
+
+    print("weights:", len(layer.weights))
+    print("trainable_weights:", len(layer.trainable_weights))
+    print("non_trainable_weights:", len(layer.non_trainable_weights))
+
+
 if __name__ == "__main__":
     print("Flower Classifier by Jarryd Stringfellow and Chase Dart")
 
     # Retrieve MobileNetV2 Data
-    mobile = tf.keras.applications.mobilenet_v2.MobileNetV2()
+    # mobile = tf.keras.applications.mobilenet_v2.MobileNetV2()
+    mobile = tf.keras.applications.mobilenet_v2.MobileNetV2(
+        input_shape=None,
+        alpha=1.0,
+        include_top=False,
+        weights='imagenet',
+        input_tensor=None,
+        pooling=None,
+        classes=1000,
+        classifier_activation='softmax'
+    )
 
     file = 'daisy/162362896_99c7d851c8_n.jpg'
+    # file = 'dandelion/8223949_2928d3f6f6_n.jpg'
+    # file = 'roses/99383371_37a5ac12a3_n.jpg'
+    # file = 'sunflowers/39271782_b4335d09ae_n.jpg'
+    # file = 'tulips/11746276_de3dec8201.jpg'
+
+    # New()
 
     preprocessed_image = PrepareImage(file)
 
