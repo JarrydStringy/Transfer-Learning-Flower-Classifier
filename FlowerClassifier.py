737ddea691366073568cb55873f33fa16f28c185
Jarryd <jarryd.stringfellow@gmail.com>
2022-06-16 21:43:28 +1000

    Cleaned and commented FlowerClassifer.py

--- FlowerClassifier.py
+++ FlowerClassifier.py
@@ -1,8 +1,5 @@
-# Imports PIL module
-from email.mime import base
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from tensorflow.keras.applications import imagenet_utils
+from sklearn.metrics import ConfusionMatrixDisplay
 from tensorflow.keras.preprocessing import image
 from sklearn.metrics import f1_score, recall_score, precision_score, accuracy_score
 from tensorflow import keras
@@ -33,14 +30,18 @@ def eval_model(model, X_train, Y_train, X_test, Y_test, X_val, Y_val):
     """
     Evaluates the model
     """
+    # Create figure and subplots
     fig = plt.figure(figsize=[25, 8])
     ax = fig.add_subplot(1, 3, 1)
+
+    # Training Set
     conf = ConfusionMatrixDisplay.from_estimator(
         model, X_train, Y_train, normalize='true', ax=ax)
     train_pred = model.predict(X_train)
     conf.ax_.set_title('Training Set Performance: ' +
                        str(sum(train_pred == Y_train)/len(Y_train)))
 
+    # Test Set
     ax = fig.add_subplot(1, 3, 2)
     conf = ConfusionMatrixDisplay.from_estimator(
         model, X_test, Y_test, normalize='true', ax=ax)
@@ -48,17 +49,13 @@ def eval_model(model, X_train, Y_train, X_test, Y_test, X_val, Y_val):
     conf.ax_.set_title('Test Set Performance: ' +
                        str(sum(test_pred == Y_test)/len(Y_test)))
 
+    # Validation Set
     ax = fig.add_subplot(1, 3, 3)
     conf = ConfusionMatrixDisplay.from_estimator(
         model, X_val, Y_val, normalize='true', ax=ax)
     val_pred = model.predict(X_val)
     conf.ax_.set_title('Validation Set Performance: ' +
                        str(sum(val_pred == Y_val)/len(Y_val)))
-
-    mins = np.min(X_train, 0)
-    maxs = np.max(X_train, 0)
-    xx, yy = np.meshgrid(np.arange(mins[0], maxs[0], 0.025),
-                         np.arange(mins[1], maxs[1], 0.025))
 
     getScores(Y_val, val_pred, "Validation Scores")
     getScores(Y_test, test_pred, "Test Scores")
@@ -70,6 +67,7 @@ def task_1():
     Global declaration of filepath to dataset
     """
     global image_path
+    # Direct path to folder location
     image_path = os.path.join(os.path.dirname(__file__),
                               'small_flower_dataset/')
 
@@ -102,6 +100,7 @@ def task_3(base_model):
     Replace the last layer with a Dense layer of the appropriate shape given that there are 5
     classes in the small flower dataset.
     """
+    # Create a Dense layer of shape 5 for each flower type
     new_predictions = keras.layers.Dense(5)(base_model.layers[-2].output)
     new_model = keras.Model(inputs=base_model.inputs, outputs=new_predictions)
 
@@ -146,33 +145,40 @@ def task_4():
             # Processes images by scaling image RGB values
             img_array_expanded_dims = np.expand_dims(img_array, axis=0)
 
-            # --------------------------------------------------------------
             y_data[qty, qty_class] = 1
             x_data[qty] = img_array_expanded_dims
             qty += 1
+
         qty_class += 1
         pictures = ()
 
+    # Split the data into training and testing
+    # The Training is 80%, Testing is 10% and Validation is 10%
+    # X refers to the data and Y refers to the labels for the data
     x_train, x_test, y_train, y_test = train_test_split(
         x_data, y_data, test_size=0.2, random_state=1)
+
     x_test, x_val, y_test, y_val = train_test_split(
         x_test, y_test, test_size=0.5, random_state=1)
+
     print(x_train)
     print(y_train)
+
     return x_train, x_test, x_val, y_train, y_test, y_val
 
 
-def task_5(model, x_train, x_test, x_val, y_train, y_test, y_val):
+def task_5(model, x_train, x_test, y_train, y_test):
     """
     Compile and train your model with an SGD3
     optimizer using the following parameters
     learning_rate=0.01, momentum=0.0, nesterov=False.
     """
-    model.compile(optimizer=keras.optimizers.SGD( learning_rate = 0.01, momentum=0.0, nesterov = False),
-              loss= 'categorical_crossentropy',
-              metrics=['accuracy'])     #sparse_categorical_crossentropy
-    
-    history = model.fit(x_train, y_train, epochs = 20, validation_data=(x_test, y_test))
+    model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.01, momentum=0.0, nesterov=False),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])  # sparse_categorical_crossentropy
+
+    history = model.fit(x_train, y_train, epochs=20,
+                        validation_data=(x_test, y_test))
 
     return history
 
@@ -182,10 +188,7 @@ def task_6(history, model, x_train, x_test, x_val, y_train, y_test, y_val):
     Plot the training and validation errors vs time as well as the training and validation
     accuracies.
     """
-    # eval_model(model, x_train, y_train, x_test, y_test, x_val, y_val)
-
-    # print(history.history.keys())
-
+    # Model accuracy plot
     plt.subplot(1, 2, 1)
     plt.plot(history.history['accuracy'])
     plt.plot(history.history['val_accuracy'])
@@ -194,6 +197,7 @@ def task_6(history, model, x_train, x_test, x_val, y_train, y_test, y_val):
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
 
+    # Model loss plot
     plt.subplot(1, 2, 2)
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -203,13 +207,9 @@ def task_6(history, model, x_train, x_test, x_val, y_train, y_test, y_val):
     plt.legend(['train'], loc='upper left')
 
     fig = plt.figure(figsize=[25, 8])
+
     # Plotting for Training
     ax = fig.add_subplot(1, 3, 1)
-    y_train_arg = np.argmax(y_train, axis=1)
-    Y_train_pred = np.argmax(model.predict(x_train), axis=1)
-    cm = confusion_matrix(y_train_arg, Y_train_pred)
-    disp = ConfusionMatrixDisplay.from_predictions(
-        y_train_arg, Y_train_pred, normalize='true', ax=ax)
     ax.xaxis.set_ticklabels(
         ['daisy', 'dandelion', 'roses', 'sunflower', 'tulips'])
     ax.yaxis.set_ticklabels(
@@ -220,11 +220,6 @@ def task_6(history, model, x_train, x_test, x_val, y_train, y_test, y_val):
 
     # Plotting for Test Data
     ax = fig.add_subplot(1, 3, 2)
-    y_test_arg = np.argmax(y_test, axis=1)
-    Y_train_pred = np.argmax(model.predict(x_test), axis=1)
-    cm = confusion_matrix(y_test_arg, Y_train_pred)
-    disp = ConfusionMatrixDisplay.from_predictions(
-        y_test_arg, Y_train_pred, normalize='true', ax=ax)
     ax.xaxis.set_ticklabels(
         ['daisy', 'dandelion', 'roses', 'sunflower', 'tulips'])
     ax.yaxis.set_ticklabels(
@@ -235,11 +230,6 @@ def task_6(history, model, x_train, x_test, x_val, y_train, y_test, y_val):
 
     # Plotting for Validation Data
     ax = fig.add_subplot(1, 3, 3)
-    y_val_arg = np.argmax(y_val, axis=1)
-    Y_val_pred = np.argmax(model.predict(x_val), axis=1)
-    cm = confusion_matrix(y_val_arg, Y_val_pred)
-    disp = ConfusionMatrixDisplay.from_predictions(
-        y_val_arg, Y_train_pred, normalize='true', ax=ax)
     ax.xaxis.set_ticklabels(
         ['daisy', 'dandelion', 'roses', 'sunflower', 'tulips'])
     ax.yaxis.set_ticklabels(
@@ -248,91 +238,97 @@ def task_6(history, model, x_train, x_test, x_val, y_train, y_test, y_val):
     ax.set_ylabel('True labels')
     ax.set_title('Validation Confusion Matrix')
 
-    # ax.show()
-    # mins = np.min(X_train, 0)
-    # maxs = np.max(X_train, 0)
-    # xx, yy = np.meshgrid(np.arange(mins[0], maxs[0], 0.025),
-    #                  np.arange(mins[1], maxs[1], 0.025))
-
-    # getScores(Y_val,val_pred, "Validation Scores")
-    # getScores(Y_test, test_pred,"Test Scores")
-
-    # plt.plot()
-
 
 def task_7(model, x_train, x_test, x_val, y_train, y_test, y_val):
     """
     Experiment with 3 different orders of magnitude for the learning rate. Plot the results, draw
     conclusions.
     """
-
+    # Base model
     model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.001, momentum=0.0, nesterov=False),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])  # sparse_categorical_crossentropy
 
-    # validation_data=(x_test, y_test),
-    history_lr1 = model.fit(x_train, y_train, epochs=10)
+    history_lr1 = model.fit(x_train, y_train, epochs=2,
+                            validation_data=(x_test, y_test))
 
+    # changed learning_rate to 0.0001
     model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.0001, momentum=0.0, nesterov=False),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])  # sparse_categorical_crossentropy
 
-    # validation_data=(x_test, y_test),
-    history_lr2 = model.fit(x_train, y_train, epochs=10)
+    history_lr2 = model.fit(x_train, y_train, epochs=2,
+                            validation_data=(x_test, y_test))
 
+    # Changed momentum to 1
     model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.01, momentum=1, nesterov=False),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])  # sparse_categorical_crossentropy
 
-    # validation_data=(x_test, y_test),
-    history_lr3 = model.fit(x_train, y_train, epochs=10)
+    history_lr3 = model.fit(x_train, y_train, epochs=2,
+                            validation_data=(x_test, y_test))
 
     return history_lr1, history_lr2, history_lr3
 
 
-def task_8(history1, history2, history3, model, x_train, x_test, x_val, y_train, y_test, y_val):
+def task_8(history1, history2, history3):
     """
     With the best learning rate that you found in the previous task, add a non zero momentum to
     the training with the SGD optimizer (consider 3 values for the momentum). Report how
     your results change.
     """
+    # Plot the results all on the one sheet
     plt.subplot(1, 2, 1)
+    # Plot history 1 accuracy
     plt.plot(history1.history['accuracy'])
     plt.plot(history1.history['val_accuracy'])
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
+
+    # Plot history 2 accuracy
     plt.plot(history2.history['accuracy'])
     plt.plot(history2.history['val_accuracy'])
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
+
+    # Plot history 3 accuracy
     plt.plot(history3.history['accuracy'])
     plt.plot(history3.history['val_accuracy'])
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
-    plt.legend(['Test: lr 0.001', 'Test: lr 0.001', 'Train: lr 0.0001', 'Test: lr 0.0001', 'Train: Momentum 1', 'Test: Momentum 1'], loc='upper left')
+
+    plt.legend(['Test: lr 0.001', 'Test: lr 0.001', 'Train: lr 0.0001',
+               'Test: lr 0.0001', 'Train: Momentum 1', 'Test: Momentum 1'], loc='upper left')
 
     plt.subplot(1, 2, 2)
+    # Plot history 1 loss
     plt.plot(history1.history['loss'])
     plt.plot(history1.history['val_loss'])
     plt.title('model loss')
     plt.ylabel('loss')
     plt.xlabel('epoch')
+
+    # Plot history 2 loss
     plt.plot(history2.history['loss'])
     plt.plot(history2.history['val_loss'])
     plt.ylabel('loss')
     plt.xlabel('epoch')
+
+    # Plot history 3 loss
     plt.plot(history3.history['loss'])
     plt.plot(history3.history['val_loss'])
     plt.ylabel('loss')
     plt.xlabel('epoch')
-    plt.legend(['Test: lr 0.001', 'Test: lr 0.001', 'Train: lr 0.0001', 'Test: lr 0.0001', 'Train: Momentum 1', 'Test: Momentum 1'], loc='upper left')
 
+    plt.legend(['Test: lr 0.001', 'Test: lr 0.001', 'Train: lr 0.0001',
+               'Test: lr 0.0001', 'Train: Momentum 1', 'Test: Momentum 1'], loc='upper left')
 
 
 if __name__ == "__main__":
     """
     Main function
+    Tasks can be commented out to filter by desired tests
     """
     print("========================================================")
     print("Flower Classifier by Jarryd Stringfellow and Chase Dart")
@@ -354,20 +350,23 @@ if __name__ == "__main__":
     x_train, x_test, x_val, y_train, y_test, y_val = task_4()
     print("\n---End of Task 4:---\n")
 
-    #print("\n---Start of Task 5:---\n")
-    #history = task_5(model, x_train, x_test, x_val, y_train, y_test, y_val)
-    #print("\n---End of Task 5:---\n")
+    print("\n---Start of Task 5:---\n")
+    history = task_5(model, x_train, x_test, y_train, y_test)
+    print("\n---End of Task 5:---\n")
 
-    #print("\n---Start of Task 6:---\n")
-    #task_6(history, model, x_train, x_test, x_val, y_train, y_test, y_val)
-    #print("\n---End of Task 6:---\n")
+    print("\n---Start of Task 6:---\n")
+    task_6(history, model, x_train, x_test, x_val, y_train, y_test, y_val)
+    print("\n---End of Task 6:---\n")
 
     print("\n---Start of Task 7:---\n")
-    history1, history2, history3 = task_7(model, x_train, x_test, x_val, y_train, y_test, y_val)
+    history1, history2, history3 = task_7(
+        model, x_train, x_test, x_val, y_train, y_test, y_val)
     print("\n---End of Task 7:---\n")
 
     print("\n---Start of Task 8:---\n")
-    task_8(history1, history2, history3, model, x_train, x_test, x_val, y_train, y_test, y_val)
+    task_8(history1, history2, history3, model, x_train,
+           x_test, x_val, y_train, y_test, y_val)
     print("\n---End of Task 8:---\n")
 
+    # Plot results
     plt.show()
